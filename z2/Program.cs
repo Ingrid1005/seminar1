b70ceab1e9c227d8f9750f9287b02060f1815c0b
Inga <tokareva10.04.5@gmail.com>
2023-09-21 19:32:40 +0200

    vse

---
+++ z2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Input first number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input thrid number");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b>max)
+{
+    max = b;
+}
+if (c>max)
+{
+    max = c;
+}
+Console.WriteLine(max);
